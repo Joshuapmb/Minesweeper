5d8ec2ccd0cd4a1acd720f9b85771420592d5e90
CallumWhitecross <43616212+CallumWhitecross@users.noreply.github.com>
2020-02-07 10:30:33 +0000

    Add extracted files via upload

--- Form1.cs
+++ Form1.cs
@@ -8,35 +8,61 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Minesweeper
+namespace WhackAMoleExample
 {
     public partial class Form1 : Form
     {
-        Button[,] btn = new Button[16, 16]; // Create 2D array of buttons
+        Button[,] btn = new Button[16, 16];
+        ContextMenuStrip menu = new ContextMenuStrip();
+        Random r = new Random();
+
         public Form1()
         {
             InitializeComponent();
-            for (int x = 0; x < btn.GetLength(0); x++) // Loop for x
+            //Controls.Add(menu);
+            for (int x = 0; x < btn.GetLength(0); x++)
             {
-                for (int y = 0; y < btn.GetLength(1); y++) // Loop for y
+                for (int y = 0; y < btn.GetLength(1); y++)
                 {
                     btn[x, y] = new Button();
-                    btn[x, y].SetBounds(30 * x, 30 * y, 30, 30);
+                    btn[x, y].SetBounds(25 * x, 25 * y, 26, 26);
                     btn[x, y].BackColor = Color.PowderBlue;
-                    btn[x, y].Text = Convert.ToString((x + 1) + "," + (y + 1));
+                    //btn[x,y].Text = Convert.ToString((x + 1) + "," + (y + 1));
                     btn[x, y].Click += new EventHandler(this.btnEvent_Click);
                     Controls.Add(btn[x, y]);
                 }
             }
-        }
-        void btnEvent_Click(object sender, EventArgs e)
-        {
-            Console.WriteLine(((Button)sender).Text); // SAME handler as before
-        }
-        private void Form1_Load(object sender, EventArgs e) //REQUIRED
-        {
-        }
 
+            int bombCount = 40;
+            while (bombCount > 0)
+            {
+                int x = r.Next(16);
+                int y = r.Next(16);
+
+
+                if (btn[x, y].BackColor != Color.Red)
+                {
+                    btn[x, y].BackColor = Color.Red;
+                    bombCount -= 1;
+                }
+
+            }
+        }
+        void btnEvent_Click(Object sender, EventArgs e)
+        {
+            if (((Button)sender).BackColor == Color.Red)
+            {
+                ((Button)sender).BackColor = Color.PowderBlue;
+                btn[r.Next(5), r.Next(5)].BackColor = Color.Red;
+                Console.WriteLine("WHACKED!");
+            }
+            else
+            {
+                Console.WriteLine("Missed!");
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
     }
 }
-            
