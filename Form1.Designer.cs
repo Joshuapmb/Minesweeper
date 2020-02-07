db7e8f0b10cc0d6b4524372a7aa7bb7e8649acec
jamesshaw <jamesshaw@labpc011>
2020-02-07 10:19:32 +0000

    Updated

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,27 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.difficultyOptionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.easyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mediumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.highscoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // menuStrip1
+            // fileToolStripMenuItem
             // 
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
+            this.fileToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.difficultyOptionsToolStripMenuItem,
+            this.resartToolStripMenuItem,
+            this.highscoresToolStripMenuItem,
+            this.exitToolStripMenuItem1});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(61, 24);
+            this.fileToolStripMenuItem.Text = "Options";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.FileToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // difficultyOptionsToolStripMenuItem
+            // 
+            this.difficultyOptionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.easyToolStripMenuItem,
+            this.mediumToolStripMenuItem,
+            this.hardToolStripMenuItem});
+            this.difficultyOptionsToolStripMenuItem.Name = "difficultyOptionsToolStripMenuItem";
+            this.difficultyOptionsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.difficultyOptionsToolStripMenuItem.Text = "Difficulty Options";
+            // 
+            // easyToolStripMenuItem
+            // 
+            this.easyToolStripMenuItem.Name = "easyToolStripMenuItem";
+            this.easyToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.easyToolStripMenuItem.Text = "Easy";
+            // 
+            // mediumToolStripMenuItem
+            // 
+            this.mediumToolStripMenuItem.Name = "mediumToolStripMenuItem";
+            this.mediumToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.mediumToolStripMenuItem.Text = "Medium";
+            // 
+            // hardToolStripMenuItem
+            // 
+            this.hardToolStripMenuItem.Name = "hardToolStripMenuItem";
+            this.hardToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.hardToolStripMenuItem.Text = "Hard";
+            // 
+            // resartToolStripMenuItem
+            // 
+            this.resartToolStripMenuItem.Name = "resartToolStripMenuItem";
+            this.resartToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.resartToolStripMenuItem.Text = "Restart";
+            // 
+            // highscoresToolStripMenuItem
+            // 
+            this.highscoresToolStripMenuItem.Name = "highscoresToolStripMenuItem";
+            this.highscoresToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.highscoresToolStripMenuItem.Text = "High Scores";
+            this.highscoresToolStripMenuItem.Click += new System.EventHandler(this.HighscoresToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.ExitToolStripMenuItem1_Click);
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.BackColor = System.Drawing.Color.Transparent;
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Padding = new System.Windows.Forms.Padding(0);
+            this.menuStrip2.Size = new System.Drawing.Size(672, 24);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.ClientSize = new System.Drawing.Size(672, 495);
+            this.Controls.Add(this.menuStrip2);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Minesweeper";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,7 +140,16 @@
 
         #endregion
 
-        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem difficultyOptionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem easyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mediumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem highscoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,7 +12,10 @@ namespace Minesweeper
 {
     public partial class Form1 : Form
     {
+        Random r = new Random();
         Button[,] btn = new Button[16, 16]; // Create 2D array of buttons
+        bool[,] bombArray = new bool[16, 16]; // Create 2D boolean bomb array
+
         public Form1()
         {
             InitializeComponent();
@@ -21,22 +24,63 @@ namespace Minesweeper
                 for (int y = 0; y < btn.GetLength(1); y++) // Loop for y
                 {
                     btn[x, y] = new Button();
-                    btn[x, y].SetBounds(30 * x, 30 * y, 30, 30);
+                    btn[x, y].SetBounds(30 * x, (30 * y)+30, 30, 30);
                     btn[x, y].BackColor = Color.PowderBlue;
                     btn[x, y].Text = Convert.ToString((x + 1) + "," + (y + 1));
-                    btn[x, y].Click += new EventHandler(this.btnEvent_Click);
+                    btn[x, y].MouseDown += new MouseEventHandler(this.btnEvent_MouseDown);
                     Controls.Add(btn[x, y]);
+
+                }
+              
+            }
+            int bombs = 40;
+            while (bombs>0)
+            {
+                int ranX = r.Next(16);
+                int ranY = r.Next(16);
+
+                if (btn[ranX, ranY].BackColor != Color.Red)
+                {
+                    btn[ranX, ranY].BackColor = Color.Red;
+                    bombs--;
                 }
             }
+            this.Size = new System.Drawing.Size(496, 550);
         }
-        void btnEvent_Click(object sender, EventArgs e)
+
+        void btnEvent_MouseDown(object sender, MouseEventArgs e)
         {
-            Console.WriteLine(((Button)sender).Text); // SAME handler as before
+            Control ctrl = ((Control)sender);
+            if (e.Button == MouseButtons.Left)
+            {
+                ctrl.BackColor = Color.Black;
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                ctrl.BackColor = Color.Beige;
+            }
+            
+            //Console.WriteLine(((Button)sender).Text); // SAME handler as before
         }
+
         private void Form1_Load(object sender, EventArgs e) //REQUIRED
         {
         }
 
+        private void FileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void HighscoresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
             
